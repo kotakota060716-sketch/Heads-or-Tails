de7ed43bb505f550960803b6a1bf8b08d0a6a904
Kotaro Yamamoto <kotakota0716.06@gmail.com>
2026-07-02 21:59:28 +0900

    User name added

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Who are you?");
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name + "!");
 
         Random rand = new Random();
 
